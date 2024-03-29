1005aae79f4440f11fc1d28acc3ec25168b5dcf8
unknown <sji004@e.ntu.edu.sg>
2024-03-29 19:28:26 +0800

    The initial runnable version, single agent can get some scores.

---
+++ src/tileworld/agent/MyAgent.java
@@ -0,0 +1,159 @@
+package tileworld.agent;
+
+import sim.util.Bag;
+import tileworld.Parameters;
+import tileworld.environment.*;
+import tileworld.exceptions.CellBlockedException;
+import tileworld.planners.AstarPathGenerator;
+import tileworld.planners.TWPath;
+import tileworld.planners.TWPathStep;
+
+public class MyAgent extends TWAgent {
+  private final String name;
+  private AstarPathGenerator pathGenerator;
+
+  public MyAgent(String name, int xpos, int ypos, TWEnvironment env, double fuelLevel) {
+    super(xpos, ypos, env, fuelLevel);
+    this.name = name;
+    // TODO maxSearchDistance设置成物体的存活时间，是否合理？
+    this.pathGenerator = new AstarPathGenerator(env, this, Parameters.lifeTime);
+    this.sensor = new MySensor(this, Parameters.defaultSensorRange, env);
+    this.memory = new MyMemory(this, env.schedule, env.getxDimension(), env.getyDimension());
+  }
+
+  protected TWThought think() {
+    if (needRefuel()) {
+      TWFuelStation fuelStation = ((MyMemory) getMemory()).getFuelStation();
+      if (fuelStation == null) {
+        System.out.println("Unlucky! Don't know where to refuel!");
+        return null;
+      }
+
+      // 如果已经在加油站了，就加油，否则生成去加油站的最短路径然后移动
+      if (this.getEnvironment().inFuelStation(this)) {
+        return new TWThought(TWAction.REFUEL, TWDirection.Z);
+      } else {
+        TWPath path = pathGenerator.findPath(this.x, this.y, fuelStation.getX(), fuelStation.getY());
+        TWPathStep step = path.popNext();
+        return new TWThought(TWAction.MOVE, step.getDirection());
+      }
+    } else {
+    	TWEntity hole = getMemory().getClosestObjectInSensorRange(TWHole.class);
+        TWEntity tile = getMemory().getClosestObjectInSensorRange(TWTile.class);
+      
+      // 处理视野里没有tile或hole的情况
+      if (tile == null && hole == null) {
+        return randomMove();
+      } else if (tile == null) {
+        if (this.hasTile()) {
+          return moveTo(hole);
+        } else {
+          return randomMove();
+        }
+      } else if (hole == null) {
+        if (this.carriedTiles.size() == 3) {
+          return randomMove();
+        } else if (this.x == tile.getX() && this.y == tile.getY()) {
+            return new TWThought(TWAction.PICKUP, TWDirection.Z);
+        } else {
+          return moveTo(tile);
+        }
+      }
+
+      // 简单规则: 手上有tile且在hole中就put，手上tile不满且在tile中就pickup，否则寻找最近的tile/hole
+      if (this.x == tile.getX() && this.y == tile.getY() && this.carriedTiles.size() < 3) {
+        return new TWThought(TWAction.PICKUP, TWDirection.Z);
+      } else if (this.x == hole.getX() && this.y == hole.getY() && this.hasTile()) {
+        return new TWThought(TWAction.PUTDOWN, TWDirection.Z);
+      } else {
+         // 离hole更近且手里有tile，或者手里tile满了，就去hole，否则去拿tile
+         if ((this.getDistanceTo(hole) <= this.getDistanceTo(tile) && this.hasTile()) || this.carriedTiles.size() == 3) {
+           return moveTo(hole);
+         } else {
+           return moveTo(tile);
+         }
+      }
+    }
+  }
+
+  // TODO decide when shall we navigate to fuelStation, need better criteria
+  private boolean needRefuel() {
+    TWFuelStation fuelStation = ((MyMemory) getMemory()).getFuelStation();
+    return fuelStation != null && fuelStation.getDistanceTo(this) * 1.5 > this.getFuelLevel();
+  }
+
+  @Override
+  protected void act(TWThought thought) {
+
+    //You can do:
+    //move(thought.getDirection())
+    //pickUpTile(Tile)
+    //putTileInHole(Hole)
+    //refuel()
+    switch (thought.getAction()) {
+      case MOVE -> {
+        try {
+          this.move(thought.getDirection());
+        } catch (CellBlockedException ex) {
+
+          // Cell is blocked, replan?
+        }
+      }
+      case PICKUP -> {
+        TWTile tile = (TWTile) getMemory().getClosestObjectInSensorRange(TWTile.class);
+        this.pickUpTile(tile);
+        this.memory.removeObject(tile);
+      }
+      case PUTDOWN -> {
+        TWHole hole = (TWHole) getMemory().getClosestObjectInSensorRange(TWHole.class);
+        this.putTileInHole(hole);
+        this.memory.removeObject(hole);
+      }
+      case REFUEL -> this.refuel();
+    }
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public void communicate() {
+    assert this.sensor instanceof MySensor;
+    Bag entities = ((MySensor) this.sensor).getSensedObjects();
+    Message message = new MyMessage("", "", "", entities);
+    this.getEnvironment().receiveMessage(message);
+  }
+
+  private TWDirection getRandomDirection() {
+    TWDirection randomDir = TWDirection.values()[this.getEnvironment().random.nextInt(5)];
+
+    if(this.getX()>=this.getEnvironment().getxDimension() ){
+      randomDir = TWDirection.W;
+    }else if(this.getX()<=1 ){
+      randomDir = TWDirection.E;
+    }else if(this.getY()<=1 ){
+      randomDir = TWDirection.S;
+    }else if(this.getY()>=this.getEnvironment().getxDimension() ){
+      randomDir = TWDirection.N;
+    }
+
+    return randomDir;
+  }
+  
+  private TWThought randomMove() {
+    return new TWThought(TWAction.MOVE, getRandomDirection());
+  }
+  
+  public TWThought moveTo(TWEntity entity) {
+	    assert entity != null;
+	    TWPath path = pathGenerator.findPath(this.x, this.y, entity.getX(), entity.getY());
+	    if (path == null) {
+	      return randomMove();
+	    } else {
+	      TWPathStep step = path.popNext();
+	      return new TWThought(TWAction.MOVE, step.getDirection());
+	    }
+	  }
+}
